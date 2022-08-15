c69169371ea1fedfb94db2a125b95a4751c79779
Gal Levy <gallevy88@gmail.com>
2022-08-15 15:14:22 +0300

    adaption ls for Rajnish data

--- Scripts/csv_to_DataFrame.py
+++ Scripts/csv_to_DataFrame.py
@@ -2,9 +2,19 @@
 import csv
 import pandas as pd
 
+def create_ls():
+    ls = []
+    for i in range (0,18):
+        ls.append(i)
+    return ls
+
 
 def adjust_csv_to_DataFrame(path):
-    df = pd.read_csv(path)
+    ls = create_ls()
+    df = pd.read_csv(path, names=ls)
+
+    # for no Rajnish Data
+    # df = pd.read_csv(path)
     return df
 
 
